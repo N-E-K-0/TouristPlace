fa6c8ad676784ec748352bb8ba19575733cf2d6d
N-E-K-0 <samiul.islam94@hotmail.com>
2020-05-03 15:02:15 +0600

    Updates

---
+++ precache-manifest.c624e37465fd2fdbb48752e8e511a643.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a297d7b9332273e9c8fc99b257b85e",
+    "url": "/TouristPlace/index.html"
+  },
+  {
+    "revision": "d92e37c9bfe9a211155e",
+    "url": "/TouristPlace/static/css/2.81faf514.chunk.css"
+  },
+  {
+    "revision": "4c9bfb792bd5940e2c3b",
+    "url": "/TouristPlace/static/css/main.5e286f4b.chunk.css"
+  },
+  {
+    "revision": "d92e37c9bfe9a211155e",
+    "url": "/TouristPlace/static/js/2.2c1368fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TouristPlace/static/js/2.2c1368fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9bfb792bd5940e2c3b",
+    "url": "/TouristPlace/static/js/main.44495aee.chunk.js"
+  },
+  {
+    "revision": "8f6caaaa589531cb710b",
+    "url": "/TouristPlace/static/js/runtime-main.eab1e72f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/TouristPlace/static/media/SEGA.12a3db8b.woff"
+  }
+]);
